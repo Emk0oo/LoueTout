7da13b7e943e65389a0867e4cee2095d7681aa94
Nathan Jourdain <nathan.jourdain50700@gmail.com>
2024-09-25 15:54:54 +0200

    modification instance settings

--- src/Entity/InstanceSettings.php
+++ src/Entity/InstanceSettings.php
@@ -4,113 +4,50 @@ namespace App\Entity;
 
 use App\Repository\InstanceSettingsRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
-use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: InstanceSettingsRepository::class)]
 class InstanceSettings
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-    #[ORM\Column(type: 'uuid', unique: true)]
-    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
-    private Uuid $id;
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    private ?string $key = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $logo = null;
+    #[ORM\Column(length: 510)]
+    private ?string $value = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $primary_color = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $secondary_color = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $tertiary_color = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $accent_color = null;
-
-    #[ORM\ManyToOne]
+    #[ORM\ManyToOne(inversedBy: 'instanceSettings')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Instance $instance = null;
 
-    public function getId(): Uuid
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getKey(): ?string
     {
-        return $this->name;
+        return $this->key;
     }
 
-    public function setName(string $name): static
+    public function setKey(string $key): static
     {
-        $this->name = $name;
+        $this->key = $key;
 
         return $this;
     }
 
-    public function getLogo(): ?string
+    public function getValue(): ?string
     {
-        return $this->logo;
+        return $this->value;
     }
 
-    public function setLogo(?string $logo): static
+    public function setValue(string $value): static
     {
-        $this->logo = $logo;
-
-        return $this;
-    }
-
-    public function getPrimaryColor(): ?string
-    {
-        return $this->primary_color;
-    }
-
-    public function setPrimaryColor(?string $primary_color): static
-    {
-        $this->primary_color = $primary_color;
-
-        return $this;
-    }
-
-    public function getSecondaryColor(): ?string
-    {
-        return $this->secondary_color;
-    }
-
-    public function setSecondaryColor(?string $secondary_color): static
-    {
-        $this->secondary_color = $secondary_color;
-
-        return $this;
-    }
-
-    public function getTertiaryColor(): ?string
-    {
-        return $this->tertiary_color;
-    }
-
-    public function setTertiaryColor(?string $tertiary_color): static
-    {
-        $this->tertiary_color = $tertiary_color;
-
-        return $this;
-    }
-
-    public function getAccentColor(): ?string
-    {
-        return $this->accent_color;
-    }
-
-    public function setAccentColor(?string $accent_color): static
-    {
-        $this->accent_color = $accent_color;
+        $this->value = $value;
 
         return $this;
     }
