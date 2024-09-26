f3cc41c1c9ad95e7035ffa23ae3d724a9253186c
MaelF2002 <maelfauquette@gmail.com>
2024-09-26 14:31:49 +0200

    les element de BBD sont affiché

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\RentHistory;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,109 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HistoryController extends AbstractController
 {
     #[Route('/history', name: 'app_history')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        $history=[
-            [
-                "brand" => "Toyota",
-                "price" => 20000,
-                "pickup" => "2023-06-15",
-                "return" => "2023-06-30"
-            ],
-            [
-                "brand" => "BMW",
-                "price" => 35000,
-                "pickup" => "2022-12-01",
-                "return" => "2023-00-00"
-            ],
-            [
-                "brand" => "Mercedes",
-                "price" => 50000,
-                "pickup" => "2021-09-10",
-                "return" => "2021-11-10"
-            ],
-            [
-                "brand" => "Audi",
-                "price" => 45000,
-                "pickup" => "2020-11-05",
-                "return" => "2020-11-10"
-            ],
-            [
-                "brand" => "BMW",
-                "price" => 35000,
-                "pickup" => "2022-12-01",
-                "return" => "2023-00-00"
-            ],
-            [
-                "brand" => "Mercedes",
-                "price" => 50000,
-                "pickup" => "2021-09-10",
-                "return" => "2021-11-10"
-            ],
-            [
-                "brand" => "Audi",
-                "price" => 45000,
-                "pickup" => "2020-11-05",
-                "return" => "2020-11-10"
-            ],
-            [
-                "brand" => "BMW",
-                "price" => 35000,
-                "pickup" => "2022-12-01",
-                "return" => "2023-00-00"
-            ],
-            [
-                "brand" => "Mercedes",
-                "price" => 50000,
-                "pickup" => "2021-09-10",
-                "return" => "2021-11-10"
-            ],
-            [
-                "brand" => "Audi",
-                "price" => 45000,
-                "pickup" => "2020-11-05",
-                "return" => "2020-11-10"
-            ],
-            [
-                "brand" => "BMW",
-                "price" => 35000,
-                "pickup" => "2022-12-01",
-                "return" => "2023-00-00"
-            ],
-            [
-                "brand" => "Mercedes",
-                "price" => 50000,
-                "pickup" => "2021-09-10",
-                "return" => "2021-11-10"
-            ],
-            [
-                "brand" => "Audi",
-                "price" => 45000,
-                "pickup" => "2020-11-05",
-                "return" => "2020-11-10"
-            ],
-            [
-                "brand" => "BMW",
-                "price" => 35000,
-                "pickup" => "2022-12-01",
-                "return" => "2023-00-00"
-            ],
-            [
-                "brand" => "Mercedes",
-                "price" => 50000,
-                "pickup" => "2021-09-10",
-                "return" => "2021-11-10"
-            ],
-            [
-                "brand" => "Audi",
-                "price" => 45000,
-                "pickup" => "2020-11-05",
-                "return" => "2020-11-10"
-            ]
-        ];
+        $hist = $em->getRepository(RentHistory::class)->findAll();
         return $this->render('history/index.html.twig', [
-            'controller_name' => 'HistoryController',
-            'historyliste' => $history,
+            'historyliste' => $hist,
         ]);
     }
 }
