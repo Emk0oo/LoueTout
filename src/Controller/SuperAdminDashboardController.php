fdecc9d3a5e46940ad63f713e805151fa54d4318
Maël Mouquet <mael.mqt@gmail.com>
2024-09-26 11:34:32 +0200

    Ajout super admin

--- src/Controller/SuperAdminDashboardController.php
+++ src/Controller/SuperAdminDashboardController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SuperAdminDashboardController extends AbstractController
 {
-    #[Route('/super/admin/dashboard', name: 'app_super_admin_dashboard')]
+    #[IsGranted("ROLE_SUPER_ADMIN", message: 'You are not allowed to access the admin dashboard.')]
+    #[Route('/super-admin/dashboard', name: 'app_super_admin_dashboard')]
     public function index(): Response
     {
         return $this->render('super_admin_dashboard/index.html.twig', [
