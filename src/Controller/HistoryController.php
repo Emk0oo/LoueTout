7923c0742f3a3d0da0ad83f9eadaefc6995f87dc
MaelF2002 <maelfauquette@gmail.com>
2024-09-27 11:52:07 +0200

    historique/user

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Services\GlobalVariableService;
+use App\Repository\RentHistoryRepository;
 use App\Entity\RentHistory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +13,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HistoryController extends AbstractController
 {
     #[Route('instance/{instance}/user/history', name: 'app_history')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, RentHistoryRepository $rhr ): Response
     {
-        $history = $em->getRepository(RentHistory::class)->findAll();
+        $history= $rhr->findBy(["rentBy"=>$this->getUser()]);
+       
         return $this->render('history/index.html.twig', [
             'historyliste' => $history,
             
