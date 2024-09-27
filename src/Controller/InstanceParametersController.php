43687bf65510035d58bf724a3ce3a8a543893bd9
Maël Mouquet <mael.mqt@gmail.com>
2024-09-27 12:05:34 +0200

    Ajout modif admin

--- src/Controller/InstanceParametersController.php
+++ src/Controller/InstanceParametersController.php
@@ -2,27 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\InstanceSettings;
+use App\Form\ModifyInstanceParametersType;
+use App\Repository\InstanceSettingsRepository;
 use App\Services\GlobalVariableService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class InstanceParametersController extends AbstractController
 {
-    public function __construct(
-        private GlobalVariableService $globalVariableService
-    )
-    {
-    }
-
     #[Route('/instance/{instance}/parameters', name: 'app_instance_parameters')]
-    public function index(): Response
+    #[IsGranted("ROLE_ADMIN", message: 'You are not allowed to access the admin dashboard.')]
+    public function index(GlobalVariableService $globalVariableService, Request $request, InstanceSettingsRepository $instanceSettingsRepository, EntityManagerInterface $em): Response
     {
-        $current_instance = $this->globalVariableService->get('current_instance');
-        dd($current_instance);
+        $current_instance = $globalVariableService->get('current_instance');
+        $instance_settings = $instanceSettingsRepository->findBy([
+            'instance' => $current_instance
+        ]);
+        
+        $accent = "";
+        $secondary = "";
+        
+        foreach($instance_settings as $setting) {
+            if($setting->getKey() === 'accent') {
+                $accent = $setting->getValue();
+            }
+            if($setting->getKey() === 'secondary') {
+                $secondary = $setting->getValue();
+            }
+        }
+
+        $form = $this->createForm(ModifyInstanceParametersType::class, null, [
+            'data' => [
+                'accent' => $accent,
+                'secondary' => $secondary
+            ]
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $accentToModify = $instanceSettingsRepository->findOneBy([
+                'instance' => $current_instance,
+                'key' => 'accent'
+            ]);
+            $accentToModify->setValue($data['accent']);
+
+            $secondaryToModify = $instanceSettingsRepository->findOneBy([
+                'instance' => $current_instance,
+                'key' => 'secondary'
+            ]);
+            $secondaryToModify->setInstance($current_instance)->setKey('secondary')->setValue($data['secondary']);
+            
+            $em->persist($accentToModify);
+            $em->persist($secondaryToModify);
+            $em->flush();
+
+            return $this->redirectToRoute('app_instance', ['instance' => $current_instance->getName()]);
+        }
 
         return $this->render('instance_parameters/index.html.twig', [
-            'controller_name' => 'InstanceParametersController',
+            'accent' => $accent,
+            'secondary' => $secondary,
+            'form' => $form->createView(),
         ]);
     }
 }
